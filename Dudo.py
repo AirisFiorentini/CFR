a0fb8df91eefca424acb1ba61e6295bbcd78a483
Tokareva Kseniia <72131965+AirisFiorentini@users.noreply.github.com>
2021-12-16 21:02:25 +0300

    Add: Khun's poker and Dudo game with pre-sampling

---
+++ Dudo.py
@@ -0,0 +1,123 @@
+import random
+from pytreemap import TreeMap
+from typing import List
+from main import Node
+
+
+class DudoTrainer:
+    def __init__(self):  # Dudo definitions
+        # Dudo definitions of 2 6-sided dice
+        self.nodeMap = TreeMap()
+        self.NUM_SIDES = 6
+        self.NUM_ACTIONS = (2 * self.NUM_SIDES) + 1
+        self.DUDO = self.NUM_ACTIONS - 1
+
+        self.claimNum = ([1] * 6) + ([2] * 6)
+        self.claimRank = [2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1]
+
+    def rollDice(self) -> List[int]:
+        return [random.randint(1, self.NUM_SIDES), random.randint(1, self.NUM_SIDES)]
+
+    def infoSetToInt(self, playerRoll: int, isClaimed: List[bool]) -> int:  # convert Dudo information set to an integer
+        infoSetNum = playerRoll
+        for a in range(self.NUM_ACTIONS - 1).__reversed__():
+            infoSetNum = 2 * infoSetNum + (1 if isClaimed[a] else 0)
+        return infoSetNum
+
+    def claimHistoryToString(self, isClaimed: List[bool]) -> str:  # convert Dudo claim history to a String
+        sb = ""
+        for a in range(self.NUM_ACTIONS):
+            if isClaimed[a]:
+                if len(sb) > 0:
+                    sb += ','
+                sb += str(self.claimNum[a])
+                sb += '*'
+                sb += str(self.claimRank[a])
+        return sb
+
+    # info set node class definitions (node class)
+    # Counterfactual regret minimization iteration
+    def cfr(self, dice: List[int], isClaimed: List[bool], p0: float, p1: float) -> float:  # history -> isClaimed
+        plays = isClaimed.count(True)
+        player = plays % 2
+        # return payoff for terminal states
+        if isClaimed[self.DUDO]:
+            doubted = self.NUM_ACTIONS - 2 - isClaimed[self.NUM_ACTIONS - 2::-1].index(True)
+            # print("doubted", doubted)
+            cN = self.claimNum[doubted]
+            cR = self.claimRank[doubted]
+            realDoubtedRankQuantity = dice.count(cR) + dice.count(1)
+            if realDoubtedRankQuantity >= cN:  # как кому посчитать прибыль? >=
+                return -1  # Dudo loses -1,
+            else:
+                # вычесть
+                return 1  # visa versa  # last stake player loses |actual - claimed|
+
+        infoSet = str(self.infoSetToInt(dice[player], isClaimed))
+        # <Get information set node or create it if nonexistent>
+        node = self.nodeMap.get(infoSet)
+        AfterTrueIndex = self.NUM_ACTIONS - isClaimed[self.NUM_ACTIONS - 1::-1].index(True) if True in isClaimed else 0
+        if node is None:  # count local num_actions
+            # print("Newnode!")
+            # node = Node(13 - AfterTrueIndex, isClaimed, dice[player])  # self.NUM_ACTIONS lower
+            node = Node(13 - AfterTrueIndex if AfterTrueIndex > 0 else 12, isClaimed, dice[player])
+            node.infoSet = infoSet  # infoSetToString
+            # output feint with ears
+            # node.die = dice[player]
+            # node.isClaimed = isClaimed
+            self.nodeMap.put(infoSet, node)
+
+        # For each action, recursively call cfr with additional history and probability
+        strategy = node.getStrategy(p0 if player == 0 else p1)
+        util = [0.0] * self.NUM_ACTIONS
+        nodeUtil = 0
+        for i in range(node.NUM_ACTIONS):   # (min(node.NUM_ACTIONS, self.NUM_ACTIONS - 1)):  # min?!
+            nextHistory = isClaimed.copy()
+            iter = AfterTrueIndex + i
+            # print(self.claimHistoryToString(isClaimed))
+            # print(node.isClaimed)
+            # print(i, iter, nextHistory)
+            nextHistory[iter] = True
+            # print(self.NUM_ACTIONS - i)
+            if player == 0:
+                util[i] = -self.cfr(dice, nextHistory, p0 * strategy[i], p1)
+            else:
+                util[i] = -self.cfr(dice, nextHistory, p0, p1 * strategy[i])
+            nodeUtil += strategy[i] * util[i]
+        # For each action, compute and accumulate counterfactual regret
+        for a in range(node.NUM_ACTIONS):
+            regret = util[a] - nodeUtil
+            node.regretSum[a] += (p1 if player == 0 else p0) * regret
+        return nodeUtil
+
+    # Train Dudo
+    def train(self, iterations: int):  # ??????????
+        util = 0.0
+        for i in range(iterations):
+            dice = self.rollDice()
+            # print("Dice", dice)
+            startClaims = [False] * self.NUM_ACTIONS
+            # print(startClaims)
+            util += self.cfr(dice, startClaims, 1, 1)
+        print("Average game value: ", util / iterations)
+        # for n in self.nodeMap.values():  # print cards + history
+        #     print(n.die, self.claimHistoryToString(n.isClaimed), n.toString(), sep='|')
+        #     print()
+        return self
+
+    def getNode(self, die: int, isClaimed: List[bool]) -> str:
+        infoSet = str(self.infoSetToInt(die, isClaimed))
+        if infoSet in self.nodeMap:
+            return self.nodeMap.get(infoSet).toString()
+        else:
+            return Node(self.NUM_ACTIONS, [False] * 13).toString()  # change properly
+
+
+# DudoTrainer main method
+if __name__ == '__main__':
+    blabla = [False, False, False, False, False, True, True, False, False, False, False, False, False]
+    TrainRes = DudoTrainer().train(1000)  # more iterations!
+    print(TrainRes.getNode(6, [False]*13))
+    print(TrainRes.getNode(3, blabla))
+    # for i in range(20):
+    #     print(TrainRes.rollDice())
---
+++ main.py
@@ -0,0 +1,147 @@
+import random
+from pytreemap import TreeMap
+from typing import List
+
+# Kuhn Poker definitions
+# PASS = 0
+# BET = 1
+# NUM_ACTIONS = 2
+
+
+class Node:  # унаследовать класс
+    # Kuhn node definitions
+    def __init__(self, NUM_ACTIONS: int, isClaimed: List[bool], die=0):
+        self.regretSum = [0.0] * NUM_ACTIONS
+        self.strategy = [0.0] * NUM_ACTIONS
+        self.strategySum = [0.0] * NUM_ACTIONS
+        self.infoSet = ""
+        self.NUM_ACTIONS = NUM_ACTIONS
+        # for output Dudo
+        self.die = die
+        self.isClaimed = isClaimed
+
+    # Get current information set mixed strategy through regret-matching
+    def getStrategy(self, realizationWeight: float) -> List[float]:
+        normalizingSum = 0.0
+        for a in range(self.NUM_ACTIONS):
+            self.strategy[a] = self.regretSum[a] if self.regretSum[a] > 0 else 0
+            normalizingSum += self.strategy[a]
+        for a in range(self.NUM_ACTIONS):
+            if normalizingSum > 0:
+                self.strategy[a] /= normalizingSum
+            else:
+                self.strategy[a] = 1.0 / self.NUM_ACTIONS
+            self.strategySum[a] += realizationWeight * self.strategy[a]
+        return self.strategy
+
+    # Get average information set mixed strategy across all training iterations
+    def getAverageStrategy(self) -> List[float]:
+        avgStrategy = [0.0] * self.NUM_ACTIONS
+        normalizingSum = 0.0
+        for a in range(self.NUM_ACTIONS):
+            normalizingSum += self.strategySum[a]
+        for a in range(self.NUM_ACTIONS):
+            if normalizingSum > 0:
+                avgStrategy[a] = self.strategySum[a] / normalizingSum
+            else:
+                avgStrategy[a] = 1.0 / self.NUM_ACTIONS
+        return avgStrategy
+
+    def toString(self) -> str:  # Get information set string representation
+        return '{:4}: {}, regret {}'.format(self.infoSet, self.getAverageStrategy(), self.regretSum)  # str(get)
+
+
+def shuffleCards(cards: List[int]):
+    for c1 in range(len(cards) - 1, 0, -1):
+        c2 = random.randint(0, c1)
+        cards[c1], cards[c2] = cards[c2], cards[c1]
+
+
+class KuhnTrainer:
+    def __init__(self):  # Kuhn Poker definitions
+        self.nodeMap = TreeMap()  # (string, node) #public TreeMap<String, Node> nodeMap = new TreeMap<String, Node>();
+        self.PASS = 0
+        self.BET = 1
+        self.NUM_ACTIONS = 2
+
+    # Information set node class definition (node class above)
+    # Counterfactual regret minimization iteration
+    def cfr(self, cards: List[int], history: str, p0: float, p1: float) -> float:
+        plays = len(history)
+        player = plays % 2
+        opponent = 1 - player
+        # return payoff for terminal states
+        if plays > 1:
+            terminalPass = history[plays - 1] == 'p'
+            doubleBet = history[plays - 2: plays] == "bb"
+            isPlayerCardHigher = cards[player] > cards[opponent]
+            if terminalPass:
+                if history == "pp":
+                    return 1 if isPlayerCardHigher else -1
+                else:
+                    return 1
+            elif doubleBet:
+                return 2 if isPlayerCardHigher else -2
+        infoSet = str(cards[player]) + history
+        # Get information set node or create it if nonexistent
+        node = self.nodeMap.get(infoSet)
+        if node is None:
+            node = Node(self.NUM_ACTIONS, [False] * 13)  # isClaimed is extra
+            node.infoSet = infoSet
+            self.nodeMap.put(infoSet, node)
+
+        # For each action, recursively call cfr with additional history and probability
+        strategy = node.getStrategy(p0 if player == 0 else p1)
+        util = [0.0] * self.NUM_ACTIONS
+        nodeUtil = 0
+        for a in range(self.NUM_ACTIONS):
+            nextHistory = history + ("p" if a == 0 else "b")
+            if player == 0:
+                util[a] = -self.cfr(cards, nextHistory, p0 * strategy[a], p1)
+            else:
+                util[a] = -self.cfr(cards, nextHistory, p0, p1 * strategy[a])
+            nodeUtil += strategy[a] * util[a]
+        # For each action, compute and accumulate counterfactual regret
+        for a in range(self.NUM_ACTIONS):
+            regret = util[a] - nodeUtil
+            node.regretSum[a] += (p1 if player == 0 else p0) * regret
+        return nodeUtil
+
+    # Train Kuhn poker
+    def train(self, iterations: int):  # train Kuhn poker
+        cards = [1, 2, 3]
+        util = 0.0
+        for i in range(iterations):
+            shuffleCards(cards)
+            util += self.cfr(cards, "", 1, 1)
+        print("Average game value: ", util / iterations)
+        for n in self.nodeMap.values():
+            print(n.toString())
+
+    def onerun(self, cards):
+        self.cfr(cards, "", 1, 1)
+        print()
+        for n in self.nodeMap.values():
+            print(n.toString())
+
+    # KuhnTrainer main method
+    def main(self):
+        iterations = 2  # 000000
+        self.train(iterations)
+
+
+if __name__ == '__main__':
+    KuhnTrainer().train(100)
+    # Kt = KuhnTrainer()
+    # Kt.onerun([3, 2, 1])
+    # Kt.onerun([3, 1, 2])
+    # Kt.onerun([1, 2, 3])
+    # Kt.onerun([2, 1, 3])
+    # Kt.onerun([1, 3, 2])
+    # Kt.onerun([2, 3, 1])
+    # Kt.onerun([3, 2, 1])
+    # Kt.onerun([3, 1, 2])
+    # Kt.onerun([1, 2, 3])
+    # Kt.onerun([2, 1, 3])
+    # Kt.onerun([1, 3, 2])
+    # Kt.onerun([2, 3, 1])
